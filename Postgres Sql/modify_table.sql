2584e217e596033a793787852c54f282c3ffcb4d
dushyantsharma460 <manuchintubhaibhai@gmail.com>
2025-08-02 19:36:41 +0530

    modify operation in postgres

--- Postgres Sql/modify_table.sql
+++ Postgres Sql/modify_table.sql
@@ -13,7 +13,7 @@ CREATE TABLE ben10 (
 ALTER TABLE ben10 RENAME TO alienforce;
 
 
--- Check all existing table (mysql -> show tables)
+-- Check all existing table (mysql -> show tables;)
 SELECT table_name 
 FROM information_schema.tables 
 WHERE table_schema = 'public' 
@@ -26,5 +26,44 @@ DROP TABLE alienforce;
 
 -- Drop table conditional 
 DROP TABLE IF EXISTS alienforce;
+
+
+-- Describe table (in terminal -> \d alienforce)  (in mysql ->  DESCRIBE alienforce;  or DESC alienforce;)
+SELECT column_name, data_type, is_nullable, column_default
+FROM information_schema.columns
+WHERE table_name = 'alienforce';
+
+
+-- Rename column of a table
+ALTER TABLE alienforce RENAME COLUMN first_appearance to finding_date;
+
+
+-- Query to delete column from table 
+ALTER TABLE alienforce DROP COLUMN species;
+
+
+-- Query to add new column (BOOL  or BOOLEAN)
+ALTER TABLE alienforce ADD COLUMN is_ultimate BOOL DEFAULT TRUE;
+
+
+-- Modify your DATATYPE and CONSTRAINT(ALTER TABLE alienforce MODIFY COLUMN alien_name VARCHAR(150) NOT NULL UNIQUE DEFAULT 'om';)
+ALTER TABLE alienforce 
+  ALTER COLUMN alien_name TYPE VARCHAR(50),
+  ALTER COLUMN alien_name SET NOT NULL,
+  ALTER COLUMN alien_name SET DEFAULT 'om';
+
+ALTER TABLE alienforce 
+  ADD CONSTRAINT unique_name UNIQUE (alien_name);
+-- Difference in NOT NULL |  UNIQUE  |  PRIMARY KEY
+-- NOT NULL IS COLUMN RELATED CONTRAINT
+-- WHILE BOTH OF THIS ARE TABLE RELATED CONTRAINT
+-- ADD CONSTRAINT pk PRIMARY KEY (alien_name);
+-- ALTER TABLE alienforce ALTER COLUMN alien_name DROP NOT NULL;
+
+
+-- Also set the order of column in mysql
+-- ALTER TABLE alienforce MODIFY COLUMN alien_id SERIAL AFTER abilities;
+
+
 
 
